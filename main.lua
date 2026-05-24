0eefc628ec298f4c1a41005a5b6b536d4815b892
Johanna Okas <johannaokas@gmail.com>
2026-05-24 21:06:39 +0300

    add movement with arrow keys

--- main.lua
+++ main.lua
@@ -1,14 +1,27 @@
 function love.load()
-  x = 50
+  x = 300
+  y = 300
   move = true
 end
   
 function love.update(dt) -- using delta time (dt) to move the rectangle at the same speed on all computers
-  if (x < 500 and move == true) then
-    x = x + 100 * dt -- ~update~ the value of x, making the rectangle move
+  if love.keyboard.isDown("right") then
+    x = x + 200 * dt -- ~update~ the value of x, making the rectangle move
+  end
+  
+  if love.keyboard.isDown("left") then
+    x = x - 200 * dt
+  end
+  
+  if love.keyboard.isDown("up") then
+    y = y - 200 * dt
+  end
+  
+  if love.keyboard.isDown("down") then
+    y = y + 200 * dt
   end
 end
 
 function love.draw()
-  love.graphics.rectangle('line', x, 50, 200, 120) -- (X, Y, W, H)
+  love.graphics.rectangle('fill', x, y, 120, 120) -- (X, Y, W, H)
 end
