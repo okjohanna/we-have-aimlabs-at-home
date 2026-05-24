530881f7f3da8e461922d481654baff0e3756bc1
Johanna Okas <johannaokas@gmail.com>
2026-05-24 22:30:20 +0300

    add tables, randomseed

--- main.lua
+++ main.lua
@@ -1,12 +1,30 @@
 function love.load()
+  -- Set random seed
+  math.randomseed(os.time())
+  
+  -- Figure initial position
   x = 300
   y = 300
+  
+  -- Is able to move
   move = true
+  
+  -- Fruits table from tutorial
+  fruits = {"apple", "banana", "plum"}
+  table.insert(fruits, "pear")
+  
+  for i,v in ipairs(fruits) do
+    print(i, v)
+  end
+  
+  -- Names for the worm
+  names = {"Jim", "Bob", "Natalie", "Wormillion"}
+  worm_name = names[math.random(#names)]
 end
   
-function love.update(dt) -- using delta time (dt) to move the rectangle at the same speed on all computers
+function love.update(dt) -- Add delta time (dt) to move the rectangle at the same speed on all computers
   if love.keyboard.isDown("right") then
-    x = x + 200 * dt -- ~update~ the value of x, making the rectangle move
+    x = x + 200 * dt -- ~Update~ the value of x, making the rectangle move
   end
   
   if love.keyboard.isDown("left") then
@@ -23,5 +41,17 @@ function love.update(dt) -- using delta time (dt) to move the rectangle at the s
 end
 
 function love.draw()
-  love.graphics.rectangle('fill', x, y, 120, 120) -- (X, Y, W, H)
+  -- Prints all the fruit in fruits under one another
+  for i,fruit in ipairs(fruits) do
+    love.graphics.print(fruit, 100, 100 + 50 * i)
+  end
+  
+  love.graphics.print(worm_name, (x + 25), (y - 5))
+  
+  -- [ --
+  -- love.graphics.print(names[math.random(#names)], (x + 25), (y - 5))
+  -- Keeps blasting through all the names. Looked fitting for a slots machine. 
+  -- ] --
+  
+  love.graphics.circle('fill', x, y, 15, 15) -- (X, Y, W, H)
 end
