b1447fb66465ef42a5543404839c6dabb0a031b2
Johanna Okas <johannaokas@gmail.com>
2026-05-24 20:58:22 +0300

    add if statement

--- main.lua
+++ main.lua
@@ -1,9 +1,12 @@
 function love.load()
   x = 50
+  move = true
 end
   
 function love.update(dt) -- using delta time (dt) to move the rectangle at the same speed on all computers
-  x = x + 15 * dt -- ~update~ the value of x, making the rectangle move
+  if (x < 500 and move == true) then
+    x = x + 100 * dt -- ~update~ the value of x, making the rectangle move
+  end
 end
 
 function love.draw()
