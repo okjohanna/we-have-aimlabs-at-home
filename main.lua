d7029b723b3f0ddf8025b298d88b459705f9f4e5
Johanna Okas <johannaokas@gmail.com>
2026-05-25 14:03:31 +0300

    add tick library

--- main.lua
+++ main.lua
@@ -1,34 +1,17 @@
-function love.load()
-  listOfRectangles = {}
+function love.load()  
+  tick = require "tick"
+  
+  drawRect = false
+  
+  tick.delay(function() drawRect = true end, 2)
 end
 
-function createRect()
-  rect = {}
-  rect.x = 300
-  rect.y = 300
-  rect.width = 70 -- (same as rect["width"] = 100)
-  rect.height = 50
-  rect.speed = 100
-  
-  -- Insert new rectangle into list
-  table.insert(listOfRectangles, rect)
-end
-  
-function love.keypressed(key)
-  if key == "space" then
-    createRect()
-  end
-end
-  
 function love.update(dt)
-  -- Iterate through list of rectangles
-  for i,v in ipairs(listOfRectangles) do
-    v.x = v.x + v.speed * dt
-  end
+  tick.update(dt)
 end
 
 function love.draw()
-  for i,v in ipairs(listOfRectangles) do
-    love.graphics.rectangle("line", v.x, v.y, v.width, v.height)
+  if drawRect then
+    love.graphics.rectangle("fill", 100, 100, 50, 30)
   end
 end
