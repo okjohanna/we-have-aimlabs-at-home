51fa0bcf5d7aae1b8a37699a8ac697546095b52a
Johanna Okas <johannaokas@gmail.com>
2026-05-25 16:22:21 +0300

    add aim reactivity exercise

--- main.lua
+++ main.lua
@@ -1,17 +1,46 @@
+local mouse = {}
+local circle = {}
+
+local defaultColor = {1, 0, 0}
+local successColor = {0, 1, 0}
+
+local modes = {"REACTIVITY", "PRECISION"}
+
 function love.load()  
-  tick = require "tick"
+  circle.x = 300
+  circle.y = 300
+  circle.radius = 10
   
-  drawRect = false
-  
-  tick.delay(function() drawRect = true end, 2)
+  love.graphics.setColor(defaultColor)
 end
 
 function love.update(dt)
-  tick.update(dt)
+  mouse.x, mouse.y = love.mouse.getPosition()
+  
+  local dx = mouse.x - circle.x
+  local dy = mouse.y - circle.y
+  
+  mouseInCircle = dx^2 + dy^2 < circle.radius^2
 end
 
 function love.draw()
-  if drawRect then
-    love.graphics.rectangle("fill", 100, 100, 50, 30)
+  if mouseInCircle then
+    love.graphics.setColor(successColor)
+  end
+  love.graphics.circle("fill", circle.x, circle.y, circle.radius)
+  
+  -- Reset color before drawing text
+  love.graphics.setColor(defaultColor)
+  love.graphics.print("Press M to change MODE", 0, 15)
+  love.graphics.print("Mouse coordinates: " .. mouse.x .. ", " .. mouse.y)
+end
+  
+function love.mousepressed(x, y, button, istouch)
+  local dx = x - circle.x
+  local dy = y - circle.y
+  
+  if button == 1 and dx^2 + dy^2 < circle.radius^2 then
+    circle.x = math.random(100, 500)
+    circle.y = math.random(100, 500)
   end
 end
